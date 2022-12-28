5bdb2da18c2c519dd9110a9113e68745f0d26a33
Phillip Tennen <pt@datatheorem.io>
2022-12-28 21:30:29 +0000

    Add some awesome feature

--- app/src/main/java/com/datatheorem/dt_sast_demo/App.java
+++ app/src/main/java/com/datatheorem/dt_sast_demo/App.java
@@ -4,5 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class App {
     public static void main(String[] args) {
+        WebView wv = new WebView(new AppCompatActivity());
+        wv.addJavascriptInterface(null, "javascriptInvoke");
+        wv.loadUrl("https://untrusted-web-content.com");
+
+        System.out.printf("OAuth refresh token: 1/DGXUA3FmzHZnjEJDvX8ynqJCALJpo6ZT3IVNK6aBYHU");
     }
 }
