da4a29a4369ea39da438fa00996f64de1a5fd138
Phillip Tennen <pt@datatheorem.io>
2023-04-26 14:14:29 +0100

    Dummy commit

--- test.java
+++ test.java
@@ -2,6 +2,7 @@ package com.example.androidcheckstest;
 
 import android.content.ContentProvider;
 
+
 public class VulnerableProvider1 extends ContentProvider {
 	public ParcelFileDescriptor openFile (Uri uri, String mode) throws FileNotFoundException {
 		File f = new File(DIR, uri.getLastPathSegment());
